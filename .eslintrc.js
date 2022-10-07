f8b6ffdc5b84b655fbc179235c197e6c3131a1ca
Munieru <20086673+munierujp@users.noreply.github.com>
2022-10-07 23:42:37 +0900

    Update ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,10 @@ module.exports = {
   extends: [
     '@munierujp/eslint-config-typescript'
   ],
-  plugins: [
-    'tsdoc'
-  ],
   parserOptions: {
     project: './tsconfig.json'
   },
   env: {
     browser: true
-  },
-  rules: {
-    'tsdoc/syntax': 'warn'
   }
 }
