f42e33e3caa1c7084d4d3e1c8eb02946ae8b3673
Munieru <20086673+munierujp@users.noreply.github.com>
2022-10-15 17:26:26 +0900

    Update rollup.config.ts

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,10 +1,10 @@
 import { readFileSync } from 'node:fs'
 import typescript from '@rollup/plugin-typescript'
-import { sync as globSync } from 'glob'
+import glob from 'glob'
 import type { RollupOptions } from 'rollup'
 import watch from 'rollup-plugin-watch'
 
-const configs: RollupOptions[] = globSync('src/**/main.ts').map(entryPath => ({
+const configs: RollupOptions[] = glob.sync('src/**/main.ts').map(entryPath => ({
   input: entryPath,
   output: {
     file: entryPath.replace(/^src\//, 'dist/').replace(/\/(.+)\/main\.ts$/, '/$1.js'),
