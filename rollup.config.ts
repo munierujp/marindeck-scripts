9dc80defe4b7bbf24e6b3011024148fc9e6da2d6
Munieru <20086673+munierujp@users.noreply.github.com>
2022-11-01 22:31:38 +0900

    Remove header (#58)
    * Remove header
    
    * Remove watch plugin
    
    * Remove rollup-plugin-watch

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,21 +1,15 @@
-import { readFileSync } from 'node:fs'
 import typescript from '@rollup/plugin-typescript'
 import glob from 'glob'
 import type { RollupOptions } from 'rollup'
-import watch from 'rollup-plugin-watch'
 
 const configs: RollupOptions[] = glob.sync('src/**/main.ts').map(entryPath => ({
   input: entryPath,
   output: {
     file: entryPath.replace(/^src\//, 'dist/').replace(/\/(.+)\/main\.ts$/, '/$1.js'),
-    format: 'iife',
-    banner: () => readFileSync(entryPath.replace(/\/main\.ts$/, '/header.txt'), 'utf8')
+    format: 'iife'
   },
   plugins: [
-    typescript(),
-    watch({
-      dir: 'src'
-    })
+    typescript()
   ]
 }))
 
